4f0adf79bbce780748c8483a8915e78538886946
gabe <gabrieltucker260@gmail.com>
2024-01-17 21:15:04 -0800

    added if elsif else statement

--- test.rb
+++ test.rb
@@ -1,5 +1,12 @@
 print "What's your first name? "
-first_name = gets.chomp.capitalize!
+first_name = gets.chomp
+if first_name.length > 6
+    first_name.upcase!
+    elsif first_name.length > 2 && first_name.length < 4
+    first_name.reverse!
+    else 
+    first_name.downcase!
+    end
 
 print "What's your last name? "
 last_name = gets.chomp.capitalize!
@@ -11,4 +18,4 @@ print "What state or province are you from? "
 state = gets.chomp.upcase!
 puts ("Your name is #{first_name}")
 puts ("Your last name is #{last_name}")
-puts ("You are from #{city} #{state}")
+puts ("You are from #{city}, #{state}")
